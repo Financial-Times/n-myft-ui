e3ecfbdbe2554c9fdf51ff0fc6729d9b01c97de2
Roger Rajaratnam <roger@sourcier.uk>
2018-02-12 12:38:28 +0000

    Improve child element event bubbling  🐿 v2.5.16

--- myft/ui/myft-buttons/init.js
+++ myft/ui/myft-buttons/init.js
@@ -7,7 +7,7 @@ import nNotification from 'n-notification';
 import Delegate from 'ftdomdelegate';
 import personaliseLinks from '../personalise-links';
 import doFormSubmit from './do-form-submit';
-import handlePinning from './pin-button';
+import pinning, {findButton} from './pin-button';
 
 const delegate = new Delegate(document.body);
 let initialised;
@@ -79,7 +79,7 @@ const pinButtonEventListeners = () => {
 	myftApiClient.init().then(() => {
 		delegate.on('click', 'button[data-prioritise-button]', event => {
 			event.preventDefault();
-			handlePinning('SPAN' === event.target.tagName ? event.target.parentNode : event.target);
+			pinning(findButton(event.target));
 		});
 	});
 };
--- myft/ui/myft-buttons/pin-button.js
+++ myft/ui/myft-buttons/pin-button.js
@@ -1,18 +1,26 @@
 const myftApiClient = require('next-myft-client');
 
-const prioritise = uuid => myftApiClient.add('user', null, 'prioritised', 'concept', uuid);
+const prioritise = uuid =>
+	myftApiClient.add('user', null, 'prioritised', 'concept', uuid);
 
-const unprioritise = uuid => myftApiClient.remove('user', null, 'prioritised', 'concept', uuid);
+const unprioritise = uuid =>
+	myftApiClient.remove('user', null, 'prioritised', 'concept', uuid);
+
+export const findButton = element =>
+	element.dataset && element.dataset.hasOwnProperty('prioritiseButton')
+		? element
+		: findButton(element.parentNode);
 
 const reloadPage = () => {
-	if(window.location) {
+	if (window.location) {
 		location.reload();
 	}
 };
 
-export default (node) => {
+export default node => {
 	const uuid = node.dataset.conceptId;
-	const action = node.dataset.prioritised === 'true' ? unprioritise : prioritise;
+	const action =
+		node.dataset.prioritised === 'true' ? unprioritise : prioritise;
 	action(uuid).then(() => {
 		reloadPage();
 	});
