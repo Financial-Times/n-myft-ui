67f95621a3d8668db32cc439c8bdfb2f16e05f58
fenglish <asuka.ochi@ft.com>
2018-04-19 18:17:03 +0100

    extract functions for common use  🐿 v2.8.0

--- myft-notification/digest-data.js
+++ myft-notification/digest-data.js
@@ -1,27 +1,8 @@
-import { json as fetchJson } from 'fetchres';
-import { fragments as teaserFragments } from '@financial-times/n-teaser/main';
-import slimQuery from './slim-query';
+import fetchDigestData from './fetch-digest-data';
+import divideUnreadRead from './divide-unread-read';
 
 const notificationDismissTime = 'timeUserClickedMyftNotification';
 const myftNotificationsEnabled = 'myftNotificationsEnabled';
-
-const hasBeenRead = (targetArticle, readArticles) => readArticles.find(readArticle => readArticle.id === targetArticle.id);
-
-const checkDigestDataExist = ({ data = {} } = {}) => {
-	if (!data || !data.user ||
-		!data.user.digest ||
-		!data.user.digest.concepts ||
-		data.user.digest.concepts.length === 0 ||
-		data.user.digest.concepts.every(concept => !concept.articles || !concept.articles.length)) {
-		return Promise.reject(new Error('myFT Digest data is not provided'));
-	}
-	return data;
-};
-
-const flattenDigestSections = data => {
-	data.user.digest.articles = data.user.digest.concepts.reduce((acc, curr) => acc.articles.concat(curr.articles));
-	return data;
-};
 
 const orderByUnreadFirst = (data) => {
 	const digestData = data.user.digest;
@@ -30,12 +11,8 @@ const orderByUnreadFirst = (data) => {
 	// reading history for past 7 days
 	const articlesUserRead = data.user.articlesFromReadingHistory ? data.user.articlesFromReadingHistory.articles : [];
 	if (articlesUserRead.length > 0) {
-		const readArticles = [];
-		const unreadArticles = [];
-		digestData.articles.forEach(article => {
-			hasBeenRead(article, articlesUserRead) ? readArticles.push(article) : unreadArticles.push(article);
-		});
-		result.articles = unreadArticles.concat(readArticles);
+		const digestDatadivided = divideUnreadRead(digestData, articlesUserRead);
+		result.articles = digestDatadivided.unreadArticles.concat(digestDatadivided.readArticles);
 	}
 
 	return result;
@@ -47,39 +24,7 @@ export default class DigestData {
 	}
 
 	fetch () {
-		const digestQuery = `
-			${teaserFragments.teaserExtraLight}
-
-			query MyFT($uuid: String!) {
-					user(uuid: $uuid) {
-						articlesFromReadingHistory {
-							articles {
-								...TeaserExtraLight
-							}
-						}
-						digest {
-							type
-							publishedDate
-							concepts {
-								id
-								prefLabel
-								url
-								articles {
-									...TeaserExtraLight
-								}
-							}
-						}
-					}
-				}
-			`;
-		const variables = { uuid: this.uuid };
-		const url = `https://next-api.ft.com/v2/query?query=${slimQuery(digestQuery)}&variables=${JSON.stringify(variables)}&source=next-front-page-myft`;
-		const options = { credentials: 'include', timeout: 5000 };
-
-		return fetch(url, options)
-			.then(fetchJson)
-			.then(checkDigestDataExist)
-			.then(flattenDigestSections)
+		return fetchDigestData(this.uuid)
 			.then(orderByUnreadFirst)
 			.then(data => {
 				this.data = data;
