4ad47f0eed457e5b7259374924e46836aa080304
fenglish <asuka.ochi@ft.com>
2018-04-20 12:48:31 +0100

    digestDatadivided => digestDataDivided  🐿 v2.8.0

--- myft-notification/digest-data.js
+++ myft-notification/digest-data.js
@@ -11,8 +11,8 @@ const orderByUnreadFirst = (data) => {
 	// reading history for past 7 days
 	const articlesUserRead = data.user.articlesFromReadingHistory ? data.user.articlesFromReadingHistory.articles : [];
 	if (articlesUserRead.length > 0) {
-		const digestDatadivided = divideUnreadRead(digestData, articlesUserRead);
-		result.articles = digestDatadivided.unreadArticles.concat(digestDatadivided.readArticles);
+		const digestDataDivided = divideUnreadRead(digestData, articlesUserRead);
+		result.articles = digestDataDivided.unreadArticles.concat(digestDataDivided.readArticles);
 	}
 
 	return result;
