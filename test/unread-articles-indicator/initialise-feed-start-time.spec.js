8f2c011e8a0d8a4d54241569de5522f6afdd8505
Alex Curtis <alex.curtis@ft.com>
2019-09-27 11:20:49 +0100

    no clock  🐿 v2.12.4

--- test/unread-articles-indicator/initialise-feed-start-time.spec.js
+++ test/unread-articles-indicator/initialise-feed-start-time.spec.js
@@ -4,7 +4,7 @@ import sinon from 'sinon';
 import fetchMock from 'fetch-mock';
 
 
-const TIME_NOW = new Date('2018-06-05T16:00:00Z');
+// const TIME_NOW = new Date('2018-06-05T16:00:00Z');
 
 const VISIT_TIME_TODAY = new Date('2018-06-05T14:00:00Z');
 
@@ -15,15 +15,15 @@ const mockSetFeedStartTime = sinon.stub();
 describe('initialiseFeedStartTime', () => {
 
 	let lastVisitTime;
-	let clock;
+	// let clock;
 
 	before(() => {
-		clock = sinon.useFakeTimers(TIME_NOW);
+		// clock = sinon.useFakeTimers(TIME_NOW);
 		fetchMock.get('begin:https://next-api.ft.com/v2/', ()=>({body: {data: {user: {lastSeenTimestamp: lastVisitTime.toISOString()}}}}));
 	});
 	after(() => {
 		fetchMock.restore();
-		clock.restore();
+		// clock.restore();
 	});
 
 	context('with no previous feed start time', () => {
