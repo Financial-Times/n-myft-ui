7a1f79110c046d717c3f64d7af10adc0f05091e2
Glynn Phillips <glynn.phillips@ft.com>
2023-08-10 16:11:15 +0100

    Create basic follow plus instant alerts button groundworks
    At this point this is just a copy of the follow button but with some of the functionality removed.
    
    This extra functionality is not needed at this point for the instant alerts variant.
    
    To avoid conflicts and bugs I think its easier to have the instant alert version as a new file with only the functionality it needs.
    
    Should the follow plsu instant alerts test be successful we will need to revisit and review all the logic.

--- components/jsx/follow-plus-instant-alerts/follow-plus-instant-alerts.jsx
+++ components/jsx/follow-plus-instant-alerts/follow-plus-instant-alerts.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
 
-export default function Example({ title }) {
+/**
+ * @typedef {object} FollowProperties
+ * @property {string} conceptId
+ * 	The ID for the concept
+ * @property {string} name
+ * 	The user facing label for the concept
+ * @property {boolean} setFollowButtonStateToSelected
+ * 	An indicator to state whether the button state should be set to selected on the server side
+ * @property {boolean} cacheablePersonalisedUrl
+ * 	An indicator to decide whether its safe to set the button state on the server side. eg. there is no cache or the cache is personalised
+ * @property {object.<string, boolean>} flags
+ * 	FT.com feature flags
+ */
+
+/**
+ * Create a follow plus instant alerts button component
+ * @public
+ * @param {FollowProperties}
+ * @returns {React.ReactElement}
+*/
+
+
+export default function FollowPlusInstantAlerts({ conceptId, name, setFollowButtonStateToSelected, cacheablePersonalisedUrl, flags }) {
+	if (!flags.myFtApiWrite) {
+		return null;
+	}
+
+	const dynamicFormAttributes = setFollowButtonStateToSelected && cacheablePersonalisedUrl ? {
+		'action': `/myft/remove/${conceptId}`,
+		'data-js-action': `/__myft/api/core/followed/concept/${conceptId}?method=delete`
+	} : {
+		'action': `/myft/add/${conceptId}`,
+		'data-js-action': `/__myft/api/core/followed/concept/${conceptId}?method=put`
+	};
+
+	const dynamicButtonAttributes = setFollowButtonStateToSelected && cacheablePersonalisedUrl ? {
+		'aria-label': `Added ${name} to myFT: click to remove`,
+		'title': `Remove ${name} from myFT`,
+		'data-alternate-label': `Add to myFT: ${name}`,
+		'aria-pressed': true,
+		'data-alternate-text': 'Add to myFT'
+	} : {
+		'aria-label': `Add ${name} to myFT`,
+		'title': `Add ${name} to myFT`,
+		'data-alternate-label': `Added ${name} to myFT: click to remove`,
+		'aria-pressed': false,
+		'data-alternate-text': 'Added'
+	};
+
+	const buttonText = setFollowButtonStateToSelected && cacheablePersonalisedUrl ?
+		'Added' :
+		'Add to myFT';
+
 	return (
-		<p>{title}</p>
+		<form
+			{...dynamicFormAttributes}
+			className="n-myft-ui n-myft-ui--follow"
+			method="GET"
+			data-myft-ui="follow"
+			data-concept-id={conceptId}>
+			<div
+				className="n-myft-ui__announcement o-normalise-visually-hidden"
+				aria-live="assertive"
+				data-pressed-text={`Now following ${name}.`}
+				data-unpressed-text={`No longer following ${name}.`}
+			></div>
+			<button
+				{...dynamicButtonAttributes}
+				className="n-myft-follow-button n-myft-follow-button--instant-alerts"
+				data-concept-id={conceptId}
+				data-trackable="follow"
+				type="submit">
+					{buttonText}
+			</button>
+		</form>
 	);
 };
