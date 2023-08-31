7baa1152c74fbf02159e76c02d047cc7eef6ef3a
Glynn Phillips <glynn.phillips@ft.com>
2023-08-31 11:11:01 +0100

    Position the preference modal relative to its trigger
    This is a basic solution that allows the preference modal to be rendered in the correct position on the page based on the button that triggered it.
    
    We have built this in a way that was quick and easy and that we know works given the constraints of the test it will be used in. If this test is successful
    and this feature should be used elsewhere much of this will need rethinking to make it more generic and reusable.
    
    Co-authored-by: ytcleon <leon.yam@ft.com>

--- components/jsx/preferences-modal/preferences-modal.jsx
+++ components/jsx/preferences-modal/preferences-modal.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 /**
- * @typedef {object} PreferencesProperties
+ * @typedef {Object} PreferencesProperties
  * @property {string[]} currentPreferences
  * Current alert preferences the user has set up.
- * @property {object.<string, boolean>} flags
- * 	FT.com feature flags
+ * @property {string} conceptId
+ * Concept id of the concept which the modal controls
+ * @property {Record<string, boolean>} flags
+ * FT.com feature flags
+ * @property {boolean} visible
+ * Controls the visibility of the modal
  */
 
 /**
@@ -14,18 +18,28 @@ import React from 'react';
  * @param {PreferencesProperties}
  * @returns {React.ReactElement}
 */
-export default function InstantAlertsPreferencesModal({ flags, currentPreferences, visible }) {
+export default function InstantAlertsPreferencesModal({ flags, conceptId, currentPreferences, visible }) {
 	if (!flags.myFtApiWrite) {
 		return null;
 	}
 
 	const formattedCurrentPreferences = currentPreferences.join(', ')
 	return (
-		<div className={`n-myft-ui__preferences-modal ${visible ? 'n-myft-ui__preferences-modal--show' : ''}`} data-component-id="myft-preferences-modal">
+		<div
+			className={`n-myft-ui__preferences-modal ${visible ? 'n-myft-ui__preferences-modal--show' : ''}`}
+			data-component-id="myft-preferences-modal"
+			data-concept-id={conceptId}
+		>
 			<div className="n-myft-ui__preferences-modal__content">
 				<span className="o-forms-input o-forms-input--checkbox">
 					<label htmlFor="receive-instant-alerts">
-						<input id="receive-instant-alerts" type="checkbox" name="receive-instant-alerts" value="receive-instant-alerts"/>
+						<input
+							id="receive-instant-alerts"
+							type="checkbox"
+							name="receive-instant-alerts"
+							value="receive-instant-alerts"
+							data-component-id="myft-preferences-modal-checkbox"
+						/>
 						<span className="o-forms-input__label n-myft-ui__preferences-modal__checkbox__message">
 							Get instant alerts for this topic
 						</span>
