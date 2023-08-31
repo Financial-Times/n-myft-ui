0be9594fb6b626ad83daf0a59778f2293ed0f708
Glynn Phillips <glynn.phillips@ft.com>
2023-08-31 12:23:57 +0100

    Prevent unfollow action on follow + instant alerts button
    The new follow + instant alerts button has a 2 click user journey for unfollows
    
    When the user clicks on a followed button it opens a menu modal
    
    Amongst other options the menu modal has a "remove" option within it
    
    So we need to prevent the default unfollow request + button state changes in this scenario
    
    Co-authored-by: ytcleon <leon.yam@ft.com>

--- components/jsx/follow-plus-instant-alerts/follow-plus-instant-alerts.jsx
+++ components/jsx/follow-plus-instant-alerts/follow-plus-instant-alerts.jsx
@@ -63,7 +63,8 @@ export default function FollowPlusInstantAlerts({ conceptId, name, csrfToken, se
 			className="n-myft-ui n-myft-ui--follow"
 			method="GET"
 			data-myft-ui="follow"
-			data-concept-id={conceptId}>
+			data-concept-id={conceptId}
+			data-myft-ui-variant="followPlusInstantAlerts">
 			<div
 				className="n-myft-ui__announcement o-normalise-visually-hidden"
 				aria-live="assertive"
