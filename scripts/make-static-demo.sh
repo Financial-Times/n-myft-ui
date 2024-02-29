e06780d860229d9d6802cbac207fbf692ed5f5af
daniel <daniel.ruiz.consultant@ft.com>
2024-02-29 14:20:10 +0100

    deployGhPages: change paths

--- scripts/make-static-demo.sh
+++ scripts/make-static-demo.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-node demos/app &
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PUBLIC_DIR="$SCRIPT_DIR/../public"
+
+
+node "$SCRIPT_DIR/../demos/app" &
 
 pid="$!"
 
@@ -8,6 +12,6 @@ sleep 10
 
 curl http://localhost:5005 | sed 's#/public/main.css#main.css#' | sed 's#href="/"#href="index.html"#' > public/index.html
 
-mv public/* .
+mv "$PUBLIC_DIR"/* .
 
 kill $pid
